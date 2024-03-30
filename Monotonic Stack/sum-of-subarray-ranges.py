eda85b880d6b540cd8f967cd3ffcf41c646c48fc
ntrnghia <nghiant2498@gmail.com>
2024-03-30 08:03:18 -0700

    update new code

--- Monotonic Stack/sum-of-subarray-ranges.py
+++ Monotonic Stack/sum-of-subarray-ranges.py
@@ -8,22 +8,20 @@ from typing import List
 
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        left_min = [len(nums)-idx for idx in range(len(nums))]
-        right_min = [1]*len(nums)
+        n = len(nums)
+        s = 0
         stack = []
-        for idx, num in enumerate(nums):
-            while stack and num < nums[stack[-1]]:
-                top = stack.pop()
-                left_min[top] = idx - top
-                right_min[idx] += right_min[top]
-            stack.append(idx)
-        left_max = [len(nums)-idx for idx in range(len(nums))]
-        right_max = [1]*len(nums)
+        for next_larger in range(n+1):
+            while stack and (next_larger == n or nums[stack[-1]] < nums[next_larger]):
+                i = stack.pop()
+                prev_larger = stack[-1] if stack else -1
+                s += nums[i] * (i - prev_larger) * (next_larger - i)
+            stack.append(next_larger)
         stack = []
-        for idx, num in enumerate(nums):
-            while stack and num > nums[stack[-1]]:
-                top = stack.pop()
-                left_max[top] = idx - top
-                right_max[idx] += right_max[top]
-            stack.append(idx)
-        return sum([nums[idx]*(left_max[idx]*right_max[idx]-left_min[idx]*right_min[idx]) for idx in range(len(nums))])
+        for next_smaller in range(n+1):
+            while stack and (next_smaller == n or nums[stack[-1]] > nums[next_smaller]):
+                i = stack.pop()
+                prev_smaller = stack[-1] if stack else -1
+                s -= nums[i] * (i - prev_smaller) * (next_smaller - i)
+            stack.append(next_smaller)
+        return s
